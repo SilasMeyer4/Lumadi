8b0b5d6a59d7d2444ac4c5c909eb27b91b758758
SilasMeyer4 <meyer-si4@web.de>
2025-08-11 01:16:23 +0200

    adjusted public header includes

--- ThreadPool/include/lumadi/threadPool.h
+++ ThreadPool/include/lumadi/threadPool.h
@@ -1,10 +1,10 @@
 #ifndef LUMADI_THREAD_POOL_H
 #define LUMADI_THREAD_POOL_H
 
-#include "IQueue.h"
-#include "IThreadPool.h"
-#include "staticThreadPool.h"
-#include "workStealingQueue.h"
-#include "concepts.h"
+#include "detail/threadPool/concepts.h"
+#include "detail/threadPool/IThreadPool.h"
+#include "detail/threadPool/staticThreadPool.h"
+#include "detail/threadPool/workStealingQueue.h"
+#include "detail/threadPool/concepts.h"
 
 #endif // LUMADI_THREAD_POOL_H
